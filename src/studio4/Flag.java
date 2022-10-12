4ab432e6e295c5c3c303f1f7ea8cfdead39edc0c
wmykytiuk <wmykytiuk@localhost>
2022-10-12 15:44:53 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,42 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setXscale(0, 24);
+		StdDraw.setYscale(0, 16);
+		
+
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 120, 0);
+		StdDraw.filledRectangle(0, 0, 25, 20);
+		
+		StdDraw.setPenColor(100, 0, 127);
+		StdDraw.filledRectangle(6, 3, 20, 1);
+		StdDraw.filledRectangle(12, 6, 20, 1);
+		StdDraw.filledRectangle(12, 12, 20, 1);
+		StdDraw.setPenColor(54, 30, 127);
+		StdDraw.filledCircle(11, 8, 7);
+		StdDraw.setPenColor(200, 200, 10);
+		StdDraw.filledCircle(11, 8, 3);
+		StdDraw.setPenColor(54, 30, 127);
+		StdDraw.filledCircle(0, 8, 2);
+		StdDraw.filledCircle(24, 8, 2);
+		
+		StdDraw.setPenColor(200, 220, 100);
+		StdDraw.filledCircle(0, 0, 1);
+		StdDraw.filledCircle(0, 16, 1);
+		StdDraw.filledCircle(24, 16, 1);
+		StdDraw.filledCircle(24, 0, 1);
+
+		StdDraw.setPenColor(225, 0, 0);
+		StdDraw.filledRectangle(10, 9, .1, .4);
+		StdDraw.filledRectangle(12, 9, .1, .4);
+		StdDraw.filledRectangle(11, 7, .5, .4);
+
+
+
+
+
+		//StdDraw.filledRectangle(8, 8, 1, 8);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -15,10 +15,36 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
+		
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		
+		if (shape .equals("rectangle")) {
+			
+			
+			StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+			if (in.nextBoolean() == true)
+			{
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(),in.nextDouble(),in.nextDouble());
+			}
+			else
+			{
+				StdDraw.rectangle(in.nextDouble(), in.nextDouble(),in.nextDouble(),in.nextDouble());
+			}
+		
+			
+		}
+		
+		
+		
+	}
+
+	private static void filledRectangle(double nextDouble) {
+		// TODO Auto-generated method stub
 		
 	}
 }
